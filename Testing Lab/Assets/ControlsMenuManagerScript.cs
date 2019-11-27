67d31c761a7eb7108f206681aeb90f0c15bfe078
JoseAngulo <alphametaghost@gmail.com>
2019-11-27 18:56:38 +0000

    Controls menu added.

---
+++ Testing Lab/Assets/ControlsMenuManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsMenuManagerScript : MonoBehaviour
+{
+    public GameObject controlsMenu;
+    public GameObject mapLayerButtons;
+
+    public void ShowControlsMenu()
+    {
+        controlsMenu.SetActive(true);
+        mapLayerButtons.SetActive(false);
+        //Time.timeScale = 0f;
+    }
+
+    public void HiddeControlsMenu()
+    {
+        controlsMenu.SetActive(false);
+        mapLayerButtons.SetActive(true);
+        //Time.timeScale = 1f;
+    }
+
+    
+
+
+}
