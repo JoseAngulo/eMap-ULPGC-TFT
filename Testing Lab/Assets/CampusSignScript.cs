f9de2bbd55d09bef4b10a07fd5cc1f6bda1862d2
JoseAngulo <alphametaghost@gmail.com>
2019-11-29 21:27:21 +0000

    CampusName sign added to every scene

---
+++ Testing Lab/Assets/CampusSignScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CampusSignScript : MonoBehaviour
+{
+    public float fromY;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RectTransform welcomeTextRect = gameObject.GetComponent<RectTransform>();
+        LeanTween.moveY(welcomeTextRect, fromY, 1f).setEaseOutBack();
+    }
+}
