040a7ceb7fa2fed034b7b842b097b7bea24ca62f
JoseAngulo <alphametaghost@gmail.com>
2019-12-18 19:11:24 +0000

    .

---
+++ Testing Lab/Assets/changeValue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class changeValue : MonoBehaviour
+{
+
+    public GameObject dropdown;
+
+    public void change()
+    {
+        Debug.Log("CAMBIANDO VALOR DEL DROPDOWN EN CÓDIGO " + dropdown.name);
+        dropdown.GetComponent<TMP_Dropdown>().value = 1;
+    }
+}
