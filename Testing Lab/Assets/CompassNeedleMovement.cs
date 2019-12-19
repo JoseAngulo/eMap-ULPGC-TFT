08c5f9375cf5dddd771ffb596dbcc2b55e6b40e4
JoseAngulo <alphametaghost@gmail.com>
2019-12-19 18:50:20 +0000

    Compass now faces to the North correctly.

--- Testing Lab/Assets/CompassNeedleMovement.cs
+++ Testing Lab/Assets/CompassNeedleMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CompassNeedleMovement : MonoBehaviour
 {
-
-    public Transform target;
+    public Transform north;
+    public Camera camera;
     private Vector3 targetDirection;
 
-
-    
+    public RectTransform compassCenter;
+       
     void Update()
     {
-        targetDirection.z = target.eulerAngles.y;
-        transform.localEulerAngles = targetDirection;
+        Vector3 camPos = camera.WorldToScreenPoint(camera.transform.position);
+        Vector3 northPos = camera.WorldToScreenPoint(north.position);
+
+        Vector3 northDirection = compassCenter.position - northPos;
+
+        float rotationZ = Mathf.Atan2(northDirection.y, northDirection.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ + 90f);
+                
     }
 }
---
+++ Testing Lab/Assets/Scripts/CompassMovement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassMovement : MonoBehaviour
+{
+
+    public Vector3 northDirection;
+    public Transform player;
+    public Quaternion targetDirection;
+
+    public RectTransform northLayer;
+    public RectTransform targetLayer;
+
+    public Transform targetPlace;
+
+    
+    void Update()
+    {
+        changeNorthDirection();
+        changeTargetDirection();
+    }
+
+    private void changeTargetDirection()
+    {
+        northDirection.z = player.eulerAngles.y;
+        northLayer.localEulerAngles = northDirection;
+    }
+
+    private void changeNorthDirection()
+    {
+        Vector3 direction = transform.position - targetPlace.position;
+        targetDirection = Quaternion.LookRotation(direction);
+
+        targetDirection.z = -targetDirection.y;
+        targetDirection.x = 0;
+        targetDirection.y = 0;
+
+        targetLayer.localRotation = targetDirection * Quaternion.Euler(northDirection);
+
+    }
+}
